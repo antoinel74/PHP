a298d27f61338bf679f69ee4f61cf5b7f90b86b6
antoinel74 <antoine.lansman@gmail.com>
2023-10-19 11:55:31 +0200

    add exercices 5

--- conditions.php
+++ conditions.php
@@ -16,7 +16,7 @@ if( $room_is_filthy == true ){
 }
 
 
-// 1.2 improved // 
+// 2 improved // 
 
 $possible_states = [2];
 $room_filthiness = $possible_states[0];
@@ -49,26 +49,123 @@ if( $now > 500 && $now < 900 ){
 }
 
 
-// 3. Display different greeting according to te user's age// 
+// 3 & 4. Display different greeting according to te user's age & gender// 
 
 echo '<form action="" method="get">
 Type your age: <input type="number" name="age"><br>
+<label>Man:</label>
+<input type="radio" name="gender" value="man">
+<label>Woman:</label>
+<input type="radio" name="gender" value="woman">
+<label>Do you speak English ?</label>
+<label>Y</label>
+<input type="radio" name="english" value="1">
+<label>N</label>
+<input type="radio" name="english" value="0">
 <input type="submit" value="Greet me now!">
 </form>';
 
-if (isset($_GET['age'])) {
-	$age = intval($_GET['age']); // Convert the age 
-	if ($age < 12) {
-		echo "Hey kiddo !";
-	} else if ($age > 12 && $age < 18){
-		echo "Hello teenager !";
-	} else if ($age > 18 && $age < 115) {
-		echo "Hello adult";
+if (isset($_GET['age']) && isset($_GET['gender']) && isset($_GET['english'])) {
+    $age = intval($_GET['age']);
+    $gender = $_GET['gender'];
+    $english = $_GET['english'];
+
+    if ($age < 12) {
+        if ($english === "1") {
+            if ($gender === "woman") {
+                echo "Hello little girl!";
+            } else {
+                echo "Hello little boy!";
+            }
+        } else {
+            if ($gender === "woman") {
+                echo "Aloha girl!";
+            } else {
+                echo "Aloha boy!";
+            }
+        }
+    } else if ($age >= 12 && $age < 18) {
+        if ($english === "1") {
+            if ($gender === "woman") {
+                echo "Hello Miss teen!";
+            } else {
+                echo "Hello mister Teen!";
+            }
+        } else {
+            if ($gender === "woman") {
+                echo "Aloha Miss teen!";
+            } else {
+                echo "Aloha mister Teen!";
+            }
+        }
+    } else if ($age >= 18 && $age < 115) {
+        if ($english === "1") {
+            if ($gender === "woman") {
+                echo "Hello Woman";
+            } else {
+                echo "Hello Man!";
+            }
+        } else {
+            if ($gender === "woman") {
+                echo "Aloha Woman";
+            } else {
+                echo "Aloha Man!";
+            }
+        }
+    } else {
+        echo "Wow, are you still alive? Are you a robot like me?";
+    }
+}
+
+// 6. The Girl Soccer Team // 
+/* echo '<h2>Want to join a girl soccer team ?</h2>
+<form action="" method="get">
+<label>Name :</label>
+<input type="text" name="soccer_name">
+Type your age: <input type="number" name="soccer_age"><br>
+<label>Are you a girl ?</label>
+<label>Y</label>
+<input type="radio" name="soccer_gender" value="woman">
+<label>N</label>
+<input type="radio" name="soccer_gender" value="man">
+<input type="submit" value="I want to join!">
+</form>';
+
+if (isset($_GET['soccer_age']) && isset ($_GET['soccer_gender'])) {
+	$soccer_age = intval($_GET['soccer_age']);
+	$soccer_gender = $_GET['soccer_gender'];
+
+	if ($soccer_age >= 20 && $soccer_age <= 40 && $soccer_gender === "woman") {
+		echo "Welcome to the team !";
 	} else {
-		echo "Wow are you still alive ? Are you a robot like me ?";
+		echo "Sorry, you don't fit the criterias.";
 	}
-} 
+} */
 
+// 7. Same as 6. but without the else // 
+echo '<h2>Want to join a girl soccer team ?</h2>
+<form action="" method="get">
+<label>Name :</label>
+<input type="text" name="soccer_name">
+Type your age: <input type="number" name="soccer_age"><br>
+<label>Are you a girl ?</label>
+<label>Y</label>
+<input type="radio" name="soccer_gender" value="woman">
+<label>N</label>
+<input type="radio" name="soccer_gender" value="man">
+<input type="submit" value="I want to join!">
+</form>';
 
+$defaultMessage = "Sorry, I don't think you fit the criteria.";
 
+if (isset($_GET['soccer_age']) && isset ($_GET['soccer_gender'])) {
+	$soccer_age = intval($_GET['soccer_age']);
+	$soccer_gender = $_GET['soccer_gender'];
+
+	if ($soccer_age >= 20 && $soccer_age <= 40 && $soccer_gender === "woman") {
+		$defaultMessage = "Welcome to the team !";
+	}
+}
+
+echo $defaultMessage;
 ?>
