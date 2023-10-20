a552b5876a2a1e3a1f56ec3def5758543861ccf6
antoinel74 <antoine.lansman@gmail.com>
2023-10-20 16:40:41 +0200

    begin for loops

---
+++ loops.php
@@ -0,0 +1,14 @@
+<?php
+
+// While loop
+$number = 1;
+while ($number <= 120) {
+    echo $number . ' ';
+    $number++;
+}
+
+
+// For loop
+for ($number = 1; $number <= 120; $number++) {
+    echo $number . ' ';
+}
