12e020a7e0d5f87bfee87e023026d7bba56cfc22
antoinel74 <antoine.lansman@gmail.com>
2023-10-23 08:56:23 +0200

    loop in a loop in a loop in a loop

--- loops.php
+++ loops.php
@@ -12,3 +12,10 @@ while ($number <= 120) {
 for ($number = 1; $number <= 120; $number++) {
     echo $number . ' ';
 }
+
+// Display array using loops 
+$collegues = ["Valentin", "Pierre", "Tom", "Matthias", "Layla", "Elisabeth", "Liwdine", "Jean-Michel"];
+
+foreach ($collegues as $value) {
+    echo $value . ' ';
+}
